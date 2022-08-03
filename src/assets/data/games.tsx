dd6a38f2c64e74e384fe3e249a870f6a116fd331
ximuli <ximuli666@163.com>
2022-08-03 21:40:11 +0800

    添加猫咪捏脸小游戏

--- src/assets/data/games.tsx
+++ src/assets/data/games.tsx
@@ -3,4 +3,5 @@ export default [
   { name: "slither", link: "http://slither.io/", description: "贪吃蛇" },
   { name: "Mikutap", link: "https://aidn.jp/mikutap/", description: "Mikutap" },
   { name: "Tetris", link: "http://farter.cn/tetr.js/", description: "俄罗斯方块" },
+  { name: "CAT", link: "https://uchinoko-maker.jp/", description: "喵咪捏脸" },
 ]
