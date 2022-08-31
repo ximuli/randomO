46c50b08e1f596d0fb1af39102f2e78a17cd5115
ximuli <ximuli666@163.com>
2022-08-31 22:22:46 +0800

    添加喂鱼小游戏

--- src/assets/data/games.tsx
+++ src/assets/data/games.tsx
@@ -4,4 +4,5 @@ export default [
   { name: "Mikutap", link: "https://aidn.jp/mikutap/", description: "Mikutap" },
   { name: "Tetris", link: "http://farter.cn/tetr.js/", description: "俄罗斯方块" },
   { name: "CAT", link: "https://uchinoko-maker.jp/", description: "喵咪捏脸" },
+  { name: "FISH", link: "https://goldfishies.com/", description: "喂鱼" },
 ]
